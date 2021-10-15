2cbb7b122c153f123b17076aab1e8b81ad7fbfa9
Sunderi Pranata <sunderipranata@Sunderis-iMac.local>
2021-10-15 16:57:39 +0700

    fix

--- auto-switch-defaultbrowser.sh
+++ auto-switch-defaultbrowser.sh
@@ -3,6 +3,6 @@ mkdir -p ~/.scripts/logs
 touch ~/.scripts/logs/auto-switch-defaultbrowser.log
 
 
-echo $(date) >> ./logs/auto-switch-defaultbrowser.log
+echo $(date) >> ~/.scripts/logs/auto-switch-defaultbrowser.log
 defaultbrowser "$1"
 osascript ~/.scripts/confirm-system-dialog.app >> ~/.scripts/logs/auto-switch-defaultbrowser.log
